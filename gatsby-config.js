878cfd10fbc4b5e6a149931923193839d2540f63
Oleksandr Samohrai <sam_gray@ukr.net>
2019-07-24 19:25:36 +0300

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     'gatsby-plugin-netlify',
+    'gatsby-plugin-netlify-cms',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
